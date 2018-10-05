f2ad87fab1d7817c7359fb38328ba6d750ff0489
Dai <twittidai@users.noreply.github.com>
2018-10-05 10:14:02 +0200

    Fix Numthreads in test script

--- test_sk.sh
+++ test_sk.sh
@@ -2,9 +2,9 @@ module load openmpi
 make clean
 make TARGET=REPARTITIONING -j
 echo "running REPARTITIONING"
-mpirun --oversubscribe -host i10se1 -n 13 ./lulesh2.0 -s 30 -repart 0 : -host i10se4 -n 14 ./lulesh2.0 -s 30 -repart 0 > repart.log
+mpirun -x OMP_NUM_THREADS=1 --oversubscribe -host i10se1 -n 13 ./lulesh2.0 -s 30 -repart 0 : -host i10se4 -n 14 ./lulesh2.0 -s 30 -repart 0 > repart.log
 
 make clean
 make TARGET=PERFORMANCE -j
 echo "running PERFORMANCE"
-mpirun --oversubscribe -host i10se1 -n 13 ./lulesh2.0 -s 30 -repart 0 : -host i10se4 -n 14 ./lulesh2.0 -s 30 -repart 0 > perf.log
+mpirun -x OMP_NUM_THREADS=1 --oversubscribe -host i10se1 -n 13 ./lulesh2.0 -s 30 -repart 0 : -host i10se4 -n 14 ./lulesh2.0 -s 30 -repart 0 > perf.log
